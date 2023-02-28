0791ab7b10fdf23cb24659ab31520f3999283dac
abdulazizpro378 <abdujazizabdi045@gmail.com>
2023-02-28 15:09:22 +0500

    jj

--- js/smth.js
+++ js/smth.js
@@ -43,3 +43,25 @@ window.addEventListener("scroll", function () {
 	}
   }
   
+
+//   ----
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("header").style.padding = "20px 0px";
+  } else {
+    document.getElementById("header").style.padding = "25px 0px";
+  }
+}
+
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.right = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.right = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
